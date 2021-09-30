3c1f5e968ea19fecd88e95dc4b34dcbf4bab56ab
ParkerTomatoes <parker.tomatoes@gmail.com>
2021-09-30 03:02:46 -0400

    Missed a file with 4 space indentation

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,27 +1,27 @@
 const common = require('./webpack.config.common');
- const { merge } = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const TerserPlugin = require("terser-webpack-plugin"); 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
-	mode: 'production',
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name].css',
-			chunkFilename: '[id].css',
-		}),
-		new BundleAnalyzerPlugin()
-	],
-	optimization: {
-		splitChunks: {
-			chunks: 'all'
-		},
-		minimize: true,
-		minimizer: [
-			new TerserPlugin(),
-			new CssMinimizerPlugin(),
-		],
-  	},
+  mode: 'production',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new BundleAnalyzerPlugin()
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    },
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+    ],
+  },
 });
